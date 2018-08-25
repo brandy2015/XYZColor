5404c8aa544f9049a84b9651baa7e40d2f15dc68
Brandy <zhangzihao@zhangzihaodeMac-mini.local>
2018-08-25 20:04:59 +0800

    Initial commit

---
+++ XYZColor/XYZColor.swift
@@ -0,0 +1,27 @@
+//
+//  XYZColor.swift
+//  XYZColor
+//
+//  Created by 张子豪 on 2018/8/25.
+//  Copyright © 2018年 zhangqian. All rights reserved.
+//
+
+import UIKit
+
+class XYZColor: NSObject {
+    
+   
+}
+
+
+public extension UIColor{
+    public static var Success = UIColor(red: 73/255, green: 210/255, blue: 67/255, alpha:0.5)
+    public static var failed = UIColor(red: 238/255, green: 48/255, blue: 7/255, alpha:0.5)
+    public static var converting = UIColor(red: 126/255, green: 219/255, blue: 212/255, alpha:0.5)
+    public static var weibo = UIColor(red: 247/255, green: 148/255, blue: 34/255, alpha:0.8)
+    public static var wechat = UIColor(red: 13/255, green: 190/255, blue: 38/255, alpha:0.8)
+    public static var GIFOut = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha:0.8)
+    
+    public static var GIFSize = UIColor(red: 247/255, green: 200/255, blue: 96/255, alpha:0.8)
+    
+}
